d8e1d891edc9fbd538f0c643daf33877392b7335
David Stilz <dastilz7@gmail.com>
2020-05-04 15:24:21 -0400

    Defect fix

--- src/models/follows.js
+++ src/models/follows.js
@@ -5,7 +5,7 @@ const knex = require('../services/knex');  // define database based on above
 // Inserts a new followed account relationship
 const insert = async (requester, followedId) => {
     return await knex.raw(
-        "INSERT INTO Follows (follower, followed, tstamp) VALUES ((SELECT idnum FROM Identity WHERE handle = ? AND password = ?),?,?)", 
+        "INSERT INTO Follows (follower, followed, tstamp) VALUES (?,?,?)", 
         [requester, followedId, new Date().toISOString().slice(0, 19).replace('T', ' ')] 
     )
     .then((data) => data[0])
